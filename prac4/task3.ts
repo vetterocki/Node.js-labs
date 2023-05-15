6aa365d39bb05324c89c32c3f6303b82c1de0b47
Vitalii Butriy <vitbutriy@gmail.com>
2023-05-15 15:44:00 +0300

    Removed unnecessary 'program' module + code refactor

--- prac4/task3.ts
+++ prac4/task3.ts
@@ -1,25 +1,14 @@
 import {AxiosResponse} from "axios";
-
 const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
-const {program} = require('commander');
 
-program
-    .requiredOption('-f, --file <filename>', 'JSON file with array of URLs')
-    .parse(process.argv);
-
-const filename: string = program.file;
+const filename: string = process.argv.slice(2)[0];
 const pagesDirname: string = path.basename(filename, '.json') + '_pages';
 fs.mkdirSync(pagesDirname, {recursive: true});
 
-axios.get(filename)
-    .then((response: { data: string[] }) => {
-        const urls: string[] = response.data;
-        return Promise.all(
-            urls.map((url: string) => axios.get(url))
-        );
-    })
+const urls: string[] = require(`./${filename}`);
+Promise.all(urls.map((url: string) => axios.get(url)))
     .then((responses: Array<AxiosResponse<string>>) => {
         responses.forEach((response: AxiosResponse<string>, index: number) => {
             const url: string = response.config.url!;
